04e88dc8ba0ae9360e84695f12acab8c72900738
mahdavifar2002 <mahdavifar2002@gmail.com>
2023-03-04 12:42:43 +0330

    add render loop

--- src/4.Hello_Window/hello_window.cpp
+++ src/4.Hello_Window/hello_window.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
 int main()
 {
 	glfwInit();
@@ -14,6 +19,7 @@ int main()
 	// for Mac OS X, uncomment the following line
 	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+	// create GLFW window
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
@@ -23,9 +29,22 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
+	// check wheter GLAD has initialized successfully
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		cout << "Failed to initialize GLAD" << endl;
 		return -1;
 	}
+
+	// inform OpenGL about window size and set callback when it's changed
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
 }
