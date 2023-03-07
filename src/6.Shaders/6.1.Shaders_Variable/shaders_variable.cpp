18044b35f3278f6d5610db663d21488f7d5318a8
mahdavifar2002 <mahdavifar2002@gmail.com>
2023-03-07 23:33:46 +0330

    add \n at the end of line in shader source

--- src/6.Shaders/6.1.Shaders_Variable/shaders_variable.cpp
+++ src/6.Shaders/6.1.Shaders_Variable/shaders_variable.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 const char* vertexShaderSource =	"#version 330 core\n"
 	"layout(location = 0) in vec3 aPos;\n"
-	"out vec4 vertexColor;"
+	"out vec4 vertexColor;\n"
 	"void main()\n"
 	"{\n"
 	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
@@ -15,7 +15,7 @@ const char* vertexShaderSource =	"#version 330 core\n"
 
 const char* fragmentShaderSource = 	"#version 330 core\n"
 	"out vec4 FragColor;\n"
-	"in vec4 vertexColor;"
+	"in vec4 vertexColor;\n"
 	"void main()\n"
 	"{\n"
 	"	FragColor = vertexColor;\n"
