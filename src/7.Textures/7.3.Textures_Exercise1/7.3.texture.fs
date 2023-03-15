4b2b3c2d906b1aaf138149b8491a7fcd3ff989e9
mahdavifar2002 <mahdavifar2002@gmail.com>
2023-03-15 15:56:42 +0330

    make the happy face reversed by changing texture coordinate

--- src/7.Textures/7.3.Textures_Exercise1/7.3.texture.fs
+++ src/7.Textures/7.3.Textures_Exercise1/7.3.texture.fs
@@ -9,6 +9,6 @@ uniform sampler2D texture2;
 void main()
 {
 	vec4 tex1 = texture(texture1, TexCoord);
-	vec4 tex2 = texture(texture2, TexCoord * 1.2 - vec2(0.1, 0.1));
+	vec4 tex2 = texture(texture2, (1-TexCoord) * 1.2 - vec2(0.1, 0.1));
 	FragColor = mix(tex1, tex2, tex2.a);
 }
