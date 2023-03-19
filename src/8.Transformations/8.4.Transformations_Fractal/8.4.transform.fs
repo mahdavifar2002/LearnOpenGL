dd34ad8ad3579c581955c86a3002a4719be7d461
mahdavifar2002 <mahdavifar2002@gmail.com>
2023-03-19 18:20:04 +0330

    draw fractal (sierpinski triangle) using recursive calls and scale/translate transforms

--- src/8.Transformations/8.4.Transformations_Fractal/8.4.transform.fs
+++ src/8.Transformations/8.4.Transformations_Fractal/8.4.transform.fs
@@ -3,12 +3,9 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+uniform sampler2D tex;
 
 void main()
 {
-	vec4 tex1 = texture(texture1, TexCoord);
-	vec4 tex2 = texture(texture2, TexCoord * 1.2 - vec2(0.1, 0.1));
-	FragColor = mix(tex1, tex2, tex2.a * 0.5);
+	FragColor = texture(tex, TexCoord);
 }
--- src/8.Transformations/8.4.Transformations_Fractal/transformations_fractal.cpp
+++ src/8.Transformations/8.4.Transformations_Fractal/transformations_fractal.cpp
@@ -27,6 +27,35 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 }
 
+// draw fractal
+void recursive_draw(Shader shader, glm::mat4 trans, float scale, int depth)
+{
+	if (depth == 0)
+	return;
+
+	unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
+	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+	// draw the triangles
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	// trans = glm::translate(trans, glm::vec3(0.5, -0.5, 0.0f));
+	// trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+	glm::mat4 trans_top = glm::translate(trans, glm::vec3(0, scale, 0.0f));
+	trans_top = glm::scale(trans_top, glm::vec3(scale));
+	recursive_draw(shader, trans_top, scale, depth - 1);
+
+	glm::mat4 trans_right = glm::translate(trans, glm::vec3(scale, -scale, 0.0f));
+	trans_right = glm::scale(trans_right, glm::vec3(scale));
+	recursive_draw(shader, trans_right, scale, depth - 1);
+
+	glm::mat4 trans_left = glm::translate(trans, glm::vec3(-scale, -scale, 0.0f));
+	trans_left = glm::scale(trans_left, glm::vec3(scale));
+	recursive_draw(shader, trans_left, scale, depth - 1);
+}
+
+
 int main()
 {
 	glfwInit();
@@ -65,15 +94,13 @@ int main()
 
 	float vertices[] = {
 		// positions         // colors		   // texture coords
-		 0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, 1.0f, 1.0f,	// top right
-		 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f, 0.0f,	// bottom right
-		-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f,	// bottom left
-		-0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f, 0.0f, 1.0f	// top left
+		-0.5f, 0.0f, 0.0f,  1.0f, 1.0f, 0.0f, -0.5f,  1.0f,	// top left
+		 0.5f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f,  1.5f,  1.0f,	// top right
+		 0.0f,-1.0f, 0.0f,  0.0f, 1.0f, 0.0f,  0.5f, -0.5f	// bottom
 	};
 
 	unsigned int indices[] = {
-		0, 1, 3,	// first triangle
-		1, 2, 3		// second triangle
+		0, 1, 2
 	};
 
 	// define vertex array object
@@ -115,35 +142,9 @@ int main()
 
 	// load and create a texture
 	// -------------------------
-	unsigned int texture1, texture2;
-	// texture 1
-	// ---------
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// set the texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// load image, create texture and generate mipmaps
-	int width, heigth, nrChannel;
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char* data = stbi_load("../../../resources/textures/container.jpg", &width, &heigth, &nrChannel, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, heigth, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		cout << "ERROR::TEXTURE::FAILED_TO_LOAD_TEXTURE_IMAGE" << endl;
-	}
-	stbi_image_free(data);
-	// texture 2
-	// ---------
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
 	// set the texture wrapping parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
@@ -151,7 +152,9 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	// load image, create texture and generate mipmaps
-	data = stbi_load("../../../resources/textures/awesomeface.png", &width, &heigth, &nrChannel, 0);
+	int width, heigth, nrChannel;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("../../../resources/textures/awesomeface.png", &width, &heigth, &nrChannel, 0);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, heigth, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
@@ -163,12 +166,12 @@ int main()
 	}
 	stbi_image_free(data);
 
-	// tell OpenGL for each sampler to which texture unit it belongs
-	shader.use();
-	shader.setInt("texture1", 0);
-	shader.setInt("texture2", 1);
+	// to use background color where texture is transparent 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// render loop
+	// -----------
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
@@ -177,7 +180,7 @@ int main()
 		// redering commands
 		
 		// clear the color buffer
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.8f, 0.75f, 0.7f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// activate the shader program
@@ -188,35 +191,11 @@ int main()
 		
 		// bind the textures
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture1);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
+		glBindTexture(GL_TEXTURE_2D, texture);
 
-		// first container
-		// ---------------
-		// set uniform for transform matrix
+		// draw sierpinski triangle fractal
 		glm::mat4 trans = glm::mat4(1.0f);
-		trans = glm::translate(trans, glm::vec3(0.5, -0.5, 0.0f));
-		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-
-		unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-
-		// draw the triangles
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-		// second container
-		// ----------------
-		// set uniform for transform matrix
-		trans = glm::mat4(1.0f);
-		trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
-		float scale = (sin(glfwGetTime()) + 2) / 3;
-		trans = glm::scale(trans, glm::vec3(scale, scale, scale));
-
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-
-		// draw the triangles
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		recursive_draw(shader, trans, 0.5, 7);
 
 		// swap the buffers and poll IO events
 		glfwSwapBuffers(window);
